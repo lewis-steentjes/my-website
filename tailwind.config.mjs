ea2c6ce9aaa45ec36c54b70c82a597f0decb2fef
Lewis Steentjes <lpsteentjes@gmail.com>
2024-05-04 13:33:36 +1200

    Add hover effect for mobile devices

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,15 +4,15 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#e8e8e8", // Blue
-        secondary: "#dadada", // Blue Grotto
-        background: "#0C2D48", // Dark Blue
+        primary: "#e8e8e8", // Grey White
+        secondary: "#dadada", // Greyer White
+        background: "#0C2D48", // Dark Blue?
         accent: "#41aab8", // Aquamarine
-        "accent-2": "#FFD53D", // Yellow
-        "content-base": "#0f110f",
-        "content-muted": "#676767",
-        success: "#00FF00",
-        error: "#FF0000",
+        "accent-2": "#FFD53D", // Yellow?
+        "content-base": "#0f110f", // Greenish Black
+        "content-muted": "#676767", // Grey
+        success: "#00FF00", // Green
+        error: "#FF0000", // Red
       },
       boxShadow: {
         emphasis: "0 0px 10px 0px rgba(0, 0, 0, 0.3)",
